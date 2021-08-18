76660d25da486d0725fa6d7390431a91a258443a
zed <90huanw@gmail.com>
2021-08-18 20:42:40 +0800

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module mysql-trans
+
+go 1.16
+
+require github.com/go-mysql-org/go-mysql v1.3.0
---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"log"
+
+	"github.com/go-mysql-org/go-mysql/canal"
+	"github.com/go-mysql-org/go-mysql/mysql"
+)
+
+type MyEventHandler struct {
+	canal.DummyEventHandler
+}
+
+func (h *MyEventHandler) OnRow(e *canal.RowsEvent) error {
+	log.Printf("%s, %s, %s, %v", e.Table.Schema, e.Table.Name, e.Action, e.Rows)
+	return nil
+}
+
+func (h *MyEventHandler) String() string {
+	return "MyEventHandler"
+}
+
+func main() {
+	cfg := canal.NewDefaultConfig()
+	cfg.Addr = "127.0.0.1:3306"
+	cfg.User = "root"
+	cfg.Password = "jenkins"
+
+	cfg.Dump.TableDB = "game_backend"
+	cfg.Dump.Tables = []string{"Sp_GameRecord", "Sp_PlayerGameHistory"}
+
+	c, err := canal.NewCanal(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Register a handler to handle RowsEvent
+	c.SetEventHandler(&MyEventHandler{})
+
+	startPos := mysql.Position{Name: "binlog.000114", Pos: 67061836}
+
+	// Start canal
+	c.RunFrom(startPos)
+}
