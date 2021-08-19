849355deaa18d5748c4039b7798e3d4f191a13e3
zed <90huanw@gmail.com>
2021-08-19 15:30:04 +0800

    add pool

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 
@@ -8,11 +9,15 @@ import (
 	"github.com/go-mysql-org/go-mysql/client"
 	"github.com/go-mysql-org/go-mysql/mysql"
 	"github.com/huandu/go-sqlbuilder"
+	golog "github.com/siddontang/go-log/log"
 )
 
 type MyEventHandler struct {
 	canal.DummyEventHandler
 }
+
+var pool = client.NewPool(golog.Debugf, 50, 100, 5, "127.0.0.1:3306", "root", "test123456", "game_backend")
+var ctx = context.Background()
 
 func (h *MyEventHandler) OnRow(e *canal.RowsEvent) error {
 	fields := []string{}
@@ -22,12 +27,6 @@ func (h *MyEventHandler) OnRow(e *canal.RowsEvent) error {
 	for ci, cc := range e.Table.Columns {
 		fields = append(fields, cc.Name)
 		values = append(values, e.Rows[len(e.Rows)-1][ci])
-	}
-
-	// establish conn
-	conn, err := client.Connect("172.31.30.220:3307", "root", "test123456", "game_backend")
-	if err != nil {
-		log.Fatal(err)
 	}
 
 	switch e.Action {
@@ -43,8 +42,13 @@ func (h *MyEventHandler) OnRow(e *canal.RowsEvent) error {
 		query, err := sqlbuilder.MySQL.Interpolate(sql, args)
 		fmt.Println(query)
 		fmt.Println(err)
-		r, err := conn.Execute(sql, args)
-		fmt.Println(r.InsertId)
+		// get conn from pool
+		conn, _ := pool.GetConn(ctx)
+		defer pool.PutConn(conn)
+		if err != nil {
+			log.Fatal(err)
+		}
+		_, err = conn.Execute(sql, args...)
 		if err != nil {
 			log.Fatal(err)
 		}
